4bd616e7244227b3a22c7509dfe8187c42ded7fd
rosiro <akumapyon@gmail.com>
2013-03-06 14:13:33 +0900

    modified:   elemeth.pl

--- elemeth.pl
+++ elemeth.pl
@@ -11,6 +11,22 @@ use JSON qw/encode_json decode_json/;
 
 my $lalah_psycommu = 'http://192.168.1.25:5000';
 
+my @psycommus = (
+    'pyscommu1',
+    'pyscommu2',
+);
+my $server_max = @psycommus;
+sub server_address {
+    my $i = 0;
+    return sub {
+        if($i >= $server_max){
+            $i = 0;
+        }
+        $i++;
+    }
+}
+my $server = server_address();
+
 my @urllist = (
     'http://www.google.co.jp/',
     'http://www.yahoo.co.jp/',
@@ -20,7 +36,7 @@ my @coro;
 for my $url (@urllist){
     push @coro, async {
         my $ua = LWP::UserAgent->new;
-        my $res = $ua->get($lalah_psycommu.'?url='.$url.'&pass=amuro');
+        my $res = $ua->get(@psycommus[$server->()].'?url='.$url.'&pass=amuro');
         if($res->is_success){
             my $decode_json = decode_json($res->content);
             print Dumper($decode_json);
